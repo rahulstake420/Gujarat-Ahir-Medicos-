b01d5ce8c76232c1a09a91db271a165d98b54499
rahulstake420 <rahulstake420@gmail.com>
2026-04-17 00:59:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,23 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
-mongoose.connect("mongodb+srv://rahulstake420_db_user:<db_password>@cluster0.yxjdutd.mongodb.net/?appName=Cluster0")
-.then(() => console.log("MongoDB Connected ✅"))
-.catch(err => console.log("MongoDB Error ❌", err));
-
 // Test route
 app.get("/", (req, res) => {
-    res.send("🚀 Gujarat Ahir Medicos API running");
+  res.send("🚀 Gujarat Ahir Medicos API running");
 });
 
-const PORT = process.env.PORT || 3000;
+// MongoDB connect
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("MongoDB Connected ✅");
 
-app.listen(PORT, () => {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
+  });
+
+})
+.catch(err => console.log("Mongo Error ❌", err));
