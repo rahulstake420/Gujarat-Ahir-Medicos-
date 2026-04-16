45a0befd6bbe58e105f3de2f70b75e61ca24c7f2
rahulstake420 <rahulstake420@gmail.com>
2026-04-17 01:59:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 // Test route
 app.get("/", (req, res) => {
